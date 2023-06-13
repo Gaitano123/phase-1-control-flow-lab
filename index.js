3dcac87519cda9cb528ffe977b45200d06ccdb04
Gaitano <gaitano.george@student.moringaschool.com>
2023-06-13 11:56:54 +0300

    Complete Assignment

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distance){
   // Write your code here!
+if (distance<=400){
+  return "This one is on me!";
 }
-
-function ternaryCheckCity(){
-  // Write your code here!
+else if(distance > 2000 && distance <=2500) {
+  return "I will gladly take your thirty bucks.";
 }
-
-function switchOnCharmFromTip(){
+else if 
+(distance >2500) {
+  return 'No can do.';
+}
+}
+function ternaryCheckCity(city){
   // Write your code here!
+  if (city=== "NYC"){
+    return "Ok, sounds good.";
+  }
+  else {
+    return "No go."
+  }
+}
+function switchOnCharmFromTip(tip){
+  // Write your code here!
+  if (tip==="generous") {
+    return "Thank you so much.";
+  }
+  else if (tip==="not as generous") {
+    return "Thank you."
+  }
+  else {
+    return "Bye."
+  }
 }
